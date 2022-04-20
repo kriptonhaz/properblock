2d394e6b7b18f59e0919ff1deef905f2495c52d4
ical10 <mochhusnir@gmail.com>
2022-04-20 13:27:45 +0700

    setup tailwindcss

--- app/root.tsx
+++ app/root.tsx
@@ -13,11 +13,11 @@ import {
   ScrollRestoration,
 } from "@remix-run/react";
 
-import tailwindStylesheetUrl from "./styles/tailwind.css";
+import styles from "./styles/app.css";
 import { getUser } from "./session.server";
 
 export const links: LinksFunction = () => {
-  return [{ rel: "stylesheet", href: tailwindStylesheetUrl }];
+  return [{ rel: "stylesheet", href: styles }];
 };
 
 export const meta: MetaFunction = () => ({
